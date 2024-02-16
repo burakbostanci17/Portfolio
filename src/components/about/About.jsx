714a698964fd2e8504d186a7e1941f2084b75422
burakbostanci17 <122093668+burakbostanci17@users.noreply.github.com>
2024-02-17 02:56:36 +0300

    End of The Project

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,61 @@
 import './about.scss'
+import PortfolioList from '../portfolioList/PortfolioList'
+import { useEffect, useState } from 'react'
+import { experiencePortfolio, educationPortfolio, licensesCertificationPortfolio, skillsPortfolio } from '../../data'
+
 
 export default function About() {
+    const [selected, setSelected] = useState('experience')
+    const [data, setData] = useState([])
+    const list = [
+        {
+            id: 'experience',
+            title: 'Experience',
+        },
+        {
+            id: 'education',
+            title: 'Education',
+        },
+        {
+            id: 'licenses&certifications',
+            title: 'Licenses & certifications',
+        },
+        {
+            id: 'skills',
+            title: 'Skills',
+        },
+    ]
+
+    useEffect(() => {
+        switch (selected) {
+            case 'experience': setData(experiencePortfolio)
+                break
+            case 'education': setData(educationPortfolio)
+                break
+            case 'licenses&certifications': setData(licensesCertificationPortfolio)
+                break
+            case 'skills': setData(skillsPortfolio)
+                break
+            default:
+                setData(experiencePortfolio)
+        }
+    }, [selected])
     return (
         <div className="about" id='about'>
-
+            <ul>
+                {list.map((item) => (
+                    <PortfolioList id={item.id} title={item.title} active={selected === item.id} setSelected={setSelected} />
+                ))}
+            </ul>
+            <div className="container">
+                {data.map((d) => (
+                    <div className="item">
+                        <h3>{d.title}</h3>
+                        <p>{d.desc}</p>
+                        <span>{d.date}</span>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,81 @@
+export const experiencePortfolio = [
+    {
+        id: 1,
+        title: 'Software Developer',
+        desc: 'Freelance',
+        date: 'Dec 2023 - Present'
+    },
+    {
+        id: 2,
+        title: 'Reserve Officer',
+        desc: 'Tuzla Infantry School Command',
+        date: 'Apr 2021 - Jan 2022'
+    },
+    {
+        id: 3,
+        title: 'Mechanical Engineer',
+        desc: 'Baylan Measuring Instruments Water & Energy Meters · Internship',
+        date: 'Jun 2017 - Jul 2017'
+    },
+    {
+        id: 4,
+        title: 'Work and Travel',
+        desc: 'Acme Markets - Produce Clerk (New Jersey)',
+        date: 'Jun 2016 - Aug 2016'
+    },
+];
+export const educationPortfolio = [
+    {
+        id: 1,
+        title: 'Ege University',
+        desc: "Bachelor's degree, Mechanical engineering",
+        date: 'Oct 2013 - Sep 2020'
+    }
+];
+export const licensesCertificationPortfolio = [
+    {
+        id: 1,
+        title: 'React - The Complete Guide (incl. React Router & Redux)',
+        desc: 'Udemy',
+        date: 'Issued Jan 2023'
+    },
+    {
+        id: 2,
+        title: 'Web Development: Zero to Advanced (HTML, CSS, Javascript)',
+        desc: 'Udemy',
+        date: 'Issued Oct 2022'
+    },
+    {
+        id: 3,
+        title: 'SQL ',
+        desc: 'Udemy',
+        date: 'Issued Jun 2022'
+    },
+];
+export const skillsPortfolio = [
+    {
+        id: 1,
+        title: 'React.js'
+    },
+    {
+        id: 2,
+        title: 'JavaScript'
+    },
+    {
+        id: 3,
+        title: 'HTML'
+    },
+    {
+        id: 4,
+        title: 'CSS'
+    },
+    {
+        id: 5,
+        title: 'SQL'
+    },
+    {
+        id: 6,
+        title: 'Oracle Apex'
+    },
+
+]
